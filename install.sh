17eb8c35f5e0cf0c7a21f7ed816b0fd2aa3bbed7
ishikura.takakazu <ishikura.takakazu@chino-js.com>
2024-04-24 18:21:56 +0900

    Packer の利用を停止しました。

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ fi
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 
-git clone --depth 1 \
-    https://github.com/wbthomason/packer.nvim \
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# git clone --depth 1 \
+#     https://github.com/wbthomason/packer.nvim \
+#     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 ln -sf ${SCRIPT_DIR}/dot.config/nvim $HOME/.config/
